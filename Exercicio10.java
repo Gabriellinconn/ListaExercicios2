1824e19065df42030c7caa74a1819aea4dfb54a9
Gabriel Lincon <glincon44@gmail.com>
2024-03-24 11:31:31 -0300

    exercicio10

---
+++ Exercicio10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void executar(){
+ Scanner num = new Scanner(System.in);
+        
+        int[]vetor1 = new int[5];
+        int[]vetor2 = new int[5];
+
+        Prompt.imprimir("Vetor 1");
+
+        for(int i = 0; i<5; i++){
+        System.out.print("Digite Algo para o vetor "+(i+1)+": ");
+        vetor1[i] = num.nextInt();
+        }
+    
+    
+        for(int i = 0;i<5;i++){
+        Prompt.imprimir(vetor2[i]);
+        }
+
+            num.close();
+    }
+}
